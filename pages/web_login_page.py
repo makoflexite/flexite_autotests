c68d95a6bf92f2ce837a18b70a9260ab20d52d31
Mariya <mako@flexite.com>
2020-08-07 20:24:16 +0300

    some changes with wait time for elements present

--- pages/web_login_page.py
+++ pages/web_login_page.py
@@ -1,9 +1,13 @@
+import pytest
+import allure
+from allure_commons.types import AttachmentType
 # from selenium.webdriver.support.ui import Select
 # from selenium import webdriver
 from .base_page import BasePage
 from .locators import WebLoginPageLocators
 
 class WebLoginPage(BasePage):
+    @allure.step("Inputting login/password on Login page")
     def input_login_password(self):
         self.browser.visit(WebLoginPageLocators.LOGIN_PAGE_LINK)
         username = self.browser.find_by_css(WebLoginPageLocators.USERNAME)
@@ -11,19 +15,23 @@ class WebLoginPage(BasePage):
         password = self.browser.find_by_css(WebLoginPageLocators.PASSWORD)
         password.fill(WebLoginPageLocators.ORDINARY_USER_PASS)
 
+    @allure.step("Choosing English language on Login page")
     def choose_english_lang(self):
         lang_combo = self.browser.find_by_css(WebLoginPageLocators.LANG_COMBOBOX)
         lang_combo.click()
         self.browser.find_by_css("[value='0']").click()
 
+    @allure.step("Checking that Login page is on Swedish language")
     def should_be_swedish_language_on_lang_label(self):
         assert self.browser.find_by_css(WebLoginPageLocators.LANG_LABEL)[2].text=="Språk", "Strings on login form are not Swedish"
 
+    @allure.step("Checking that 'Svenska' is chosen as language on Login page")
     def should_be_swedish_language_in_lang_combobox(self):
         lang_combo = self.browser.find_by_css(WebLoginPageLocators.LANG_COMBOBOX)
         lang_combo.click()
         assert self.browser.find_by_css("[selected]").text == 'Svenska', "Not Swedish language is choosen on language combo box"
 
+    @allure.step("Pressing Login btn on Login page")
     def press_login_btn(self):
         if self.browser.is_element_visible_by_css(WebLoginPageLocators.LOGIN_BTN, wait_time=5):
             self.browser.find_by_css(WebLoginPageLocators.LOGIN_BTN).click()
--- test_login_page.py
+++ test_login_page.py
@@ -9,6 +9,7 @@ import time
 from .pages.web_login_page import WebLoginPage
 from .pages.full_web_page import FullWebPage
 
+@pytest.mark.testdebug
 def test_ordinary_user_can_login_to_web_with_swedish_def_lang(browser):
     page = WebLoginPage(browser)
     page.input_login_password()
@@ -30,13 +31,12 @@ def test_ordinary_user_can_login_to_web_with_eng_lang(browser):
     page.choose_english_lang()
     time.sleep(2) #! think of how to avoid time.sleep() function. Need to add explicit wait somewhere.
     page.press_login_btn()
-
     page1 = FullWebPage(browser)
     page1.should_be_web_loaded_with_left_menu()
     page1.should_be_correct_username()
     page1.should_be_english_strings()
+    # time.sleep(3)
     page1.go_to_settings()
-
     page2 = FullWebPage(browser)
     page2.should_be_english_language_in_settings()
 
