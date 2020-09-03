f012d793c3c33fe4b0769e226788247e95ed07a1
Mariya <mako@flexite.com>
2020-09-03 18:21:43 +0300

    added test

--- pages/web_initiate_page.py
+++ pages/web_initiate_page.py
@@ -13,15 +13,23 @@ class WebInitiatePage(BasePage):
         initiate = self.browser.find_by_css(FullWebPageLocators.PROCESS_1_INITIATE).click()
 
     @allure.step
-    def process_1_initiate_finish(self):
+    def process_1_initiate_next_click(self):
         if self.browser.is_element_present_by_text('Process 1 - Initiate', wait_time=5):
             with self.browser.get_iframe(1) as iframe:
                 assert iframe.is_element_present_by_css("#ButtonsFrame", wait_time=5), "ButtonsFrame is not found!"
                 with iframe.get_iframe('ButtonsFrame') as iframe1:
                     next_btn = iframe1.find_by_css(FullWebPageLocators.PROCESS_1_INITIATE_NEXT_BUTTON).click()
-                    if self.browser.is_element_present_by_text('E-mail receivers', wait_time=5):
-                        next_btn = iframe1.find_by_css(FullWebPageLocators.PROCESS_1_INITIATE_NEXT_BUTTON).click()
+                    # if self.browser.is_element_present_by_text('E-mail receivers', wait_time=5):
+                    #     next_btn = iframe1.find_by_css(FullWebPageLocators.PROCESS_1_INITIATE_NEXT_BUTTON).click()
 
     @allure.step
-    def process_1_fill_forms(self):
-        pass
+    def process_1_fill_form_1(self):
+        # label1 = self.browser.find_by_css(Process1InitiatePageLocators.LABEL1).click()
+        # proceed opening of new tab with link and returning back
+
+        # assert self.browser.is_element_visible_by_css(Process1InitiatePageLocators.LABEL2, wait_time=5), 'label2 is not visible but should be'
+        if self.browser.is_element_present_by_text('StartForm', wait_time=5):
+            string1 = self.browser.find_by_css(Process1InitiatePageLocators.STRING1)
+            assert string1.text == 'user00', 'macro %%%USER_NAME works incorrectly'
+            string2 = self.browser.find_by_css(Process1InitiatePageLocators.STRING2)
+            string2.fill('test value string2')
